1d0c39ad06404df4faf6923ec1c637a6a34817df
Mieka Fukui <fukum@MiekanoMacBook-Pro.local>
2022-02-04 16:20:23 +0900

    ファイル数修正

--- JavaScript assignment/main.js
+++ JavaScript assignment/main.js
@@ -26,8 +26,6 @@ $(document).ready(function() {
     $("#stop-button").prop("disabled", true);
     $(this).prop("disabled", true);
   });
-
-
 });
 
 function runTimer() {
@@ -40,3 +38,39 @@ function runTimer() {
 
   $("#time").text(min + ":" + secLarge + ":" + secSmall + ":" + miliSec);
 }
+
+function fresh(target) {
+  let display = document.getElementById("display");
+  let elementValue = target.innerHTML;
+  if (elementValue == "AC") {
+    display.innerHTML = 0;
+  } else if (elementValue == "=") {
+    display.innerHTML = eval(display.innerHTML);
+  } else {
+    if (display.innerHTML == "0" || display.innerHTML == "00") {
+      display.innerHTML = elementValue;
+    } else {
+      display.innerHTML += elementValue;
+    }
+  }
+}
+
+$(document).ready(function() {
+  $(".cha-btn").click(function() {
+    $(".sign-btn").prop("disabled", false);
+  });
+
+  $(".reset-btn").click(function() {
+    $(".sign-btn").prop("disabled", true);
+    $(".dot-btn").prop("disabled", false);
+  });
+
+  $(".sign-btn").click(function() {
+    $(".sign-btn").prop("disabled", true);
+    $(".dot-btn").prop("disabled", false);
+  });
+
+  $(".dot-btn").click(function() {
+    $(".dot-btn").prop("disabled", true);
+  });
+});
